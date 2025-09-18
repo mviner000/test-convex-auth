bede26d5324ecf608c8fbacfb358a7ebb6f8b2e1
mviner000 <m.viner001@gmail.com>
2025-09-18 18:19:35 +0800

    Spinning Loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import {
-  Authenticated,
-  Unauthenticated,
-  useConvexAuth,
-  useMutation,
-  useQuery,
-} from "convex/react";
-import { api } from "../convex/_generated/api";
+import { Authenticated, Unauthenticated, useConvexAuth } from "convex/react";
 import { useAuthActions } from "@convex-dev/auth/react";
 import { useState } from "react";
 import { Button } from "./components/ui/button";
---
+++ src/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export function LoadingSpinner() {
+  return (
+    <div className="mx-auto">
+      <p>loading... (consider a loading skeleton)</p>
+    </div>
+  );
+}
